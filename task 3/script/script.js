4df67c30461eba6f0edfd390b1c8dcb7e27abdc6
1Donat1 <alexrumenige@gmail.com>
2022-02-16 18:20:06 +0200

    Create script.js

---
+++ task 3/script/script.js
@@ -0,0 +1,12 @@
+
+
+let name='John';
+let admin= name;
+alert(admin);
+
+let ourPlanetName='Earth';
+console.log(ourPlanetName);
+let websiteName='Html.org';
+console.log(websiteName);
+let task3='10'+10;
+console.log(task3);
